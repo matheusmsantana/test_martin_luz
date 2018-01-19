045733e3923dfbdf0f6f0a34feaebe91f857dab4
matheusmsantana <matheus.marques.santana@hotmail.com>
2018-01-19 13:50:21 -0200

    Desafio

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1"/>
+		<title>Economatica</title>
+		<link rel="icon" href="imagens/favicon.ico"/>
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<link rel="stylesheet" href="estilocss/estilo.css"/>
+		<link rel="stylesheet" media="only screen and (min-width: 769px) and (max-width: 980px)" type="text/css" href="incs/css/mobile05.css"/>
+		<link rel="stylesheet" media="only screen and (min-width: 641px) and (max-width: 768px)" type="text/css" href="estilocss/mobile04.css"/>
+		<link rel="stylesheet" media="only screen and (min-width: 481px) and (max-width: 640px)" type="text/css" href="estilocss/mobile03.css"/>
+		<link rel="stylesheet" media="only screen and (min-width: 321px) and (max-width: 480px)" type="text/css" href="estilocss/mobile02.css"/>
+		<link rel="stylesheet" media="only screen and (min-width: 0px) and (max-width: 320px)" type="text/css" href="estilocss/mobile01.css"/>
+	</head>
+	<body>
+		<nav class="navbar navbar-default navbar-fixed-top menunav">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<a href="" class="navbar-brand">
+						<span class="img-logo"><p class="Decisiva">DECISIVA</p></span>
+					</a>
+					
+				</div>
+				
+				<div>
+					<span class="nav navbar-nav navbar-right">
+						<span class="Rectangle-menu"><a href="" class="Solicite">SOLICITE UMA DEMONSTRAÇÃO</a></span>
+					</span>
+				</div>
+				
+			</div>
+		</nav>
+		
+		
+		<div class="row escopo">
+	  		<div style="display:block;position:relative;" class="col-sm-11 banner">
+		  		<img class="img-responsive img-banner" src="imagens/banner.png"/>
+			
+		  		<div class="txt-banner">
+					<span class="Rectangle1"><a href="" class="Solicite-banner">SOLICITE UMA DEMONSTRAÇÃO</a></span>
+					<span><img class="img-responsive logo-banner" src="imagens/logo.png"/><p class="Decisiva1">DECISIVA</p></span>
+		  				<h1 class="Publique">Publique papers de alto desempenho e qualidade</h1> 
+		  					<p class="Entrar-contato">Entrar em contato com a realidade financeira é uma arma poderosa para qualquer aluno da área financeira,
+								da graduação aos cursos mais avançados.
+								Para decifrar o mercado, interpretar o cenário, embasar suas decisões,
+								criar valor e estar um passo à frente, os melhores gestores e analistas da indústria de fundos e ações usam a Economatica.</p>
+		 	 					<div class="Rectangle"><a href="" class="Solicite-text">SOLICITE UMA DEMONSTRAÇÃO</a></div>  
+				<div/>	
+			</div>	
+		</div>
+			
+			
+			<div class="container conteudodiv">		
+				<div class="row">
+					<div class="col-md-12">	
+						
+						<div class="col-md-6 img-conteudo">
+							<img class="img-responsive img-conteudo" src="imagens/img.png"/>
+						</div>
+						<div class="col-md-6 Rectangle-3">
+							<div>
+								<h1 class="plataforma">Uma plataforma extremamente potente e amigável para alunos e professores</h1>
+								
+								<p class="ferramenta">Uma ferramenta poderosa de Data Mining, constantemente alimentada,
+									que permite ao usuário manipular um grande volume de dados,
+									criar insights, fazer simulações avançadas,
+									gerar relatórios e comparar ativos para decidir melhor.
+								</p>
+								
+								<div class="Rectangle-ferramenta"><a href="" class="Solicite-ferramente">SOLICITE UMA DEMONSTRAÇÃO</a></div>
+								
+							</div>
+						</div>
+						
+					</div>
+				</div>	
+			<div/>		
+		
+		
+			
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<!-- Include all compiled plugins (below), or include individual files as needed -->
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
